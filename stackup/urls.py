3c87d527b04ee00d12d414b95e6ed8798a28dd24
Ryan Misura <misura@wisc.edu>
2014-02-01 00:55:05 -0600

    Stupid About page

--- stackup/urls.py
+++ stackup/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from stackup.apps.main.views import Home, Salary
+from stackup.apps.main.views import Home, Salary, About
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name="home"),
     url(r'^salary/$', Salary.as_view(), name="salary"),
+    url(r'^about/$', About.as_view(), name="about"),
     url(r'^admin/', include(admin.site.urls)),
 )
