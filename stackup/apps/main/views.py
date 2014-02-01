77b3140f64b0528bd1e36f3e54ec8aa6631d653c
James Pulec <jpulec@gmail.com>
2014-01-31 21:31:47 -0600

    Starting on form action

--- stackup/apps/main/views.py
+++ stackup/apps/main/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from stackup.apps.main.forms import SalaryForm
 
@@ -9,3 +11,11 @@ class Home(FormView):
 
 class Salary(TemplateView):
     template_name = "main/salary.html"
+
+    def get(self, request, *args, **kwargs):
+        if "salary" in self.request.GET and self.request.GET['salary']:
+            pass
+        else:
+            return HttpResponseRedirect(reverse("home"))
+        return super(Salary, self).get(request, *args, **kwargs)
+
