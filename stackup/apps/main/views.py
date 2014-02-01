084454c2bbac6694062b5b60354a85a96c9f217a
James Pulec <jpulec@gmail.com>
2014-01-31 21:10:20 -0600

    Add landing page

--- stackup/apps/main/views.py
+++ stackup/apps/main/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
-class Home(TemplateView):
+from stackup.apps.main.forms import SalaryForm
+
+class Home(FormView):
     template_name = "main/home.html"
+    form_class = SalaryForm
+
+class Salary(TemplateView):
+    template_name = "main/salary.html"
--- stackup/urls.py
+++ stackup/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from stackup.apps.main.views import Home
+from stackup.apps.main.views import Home, Salary
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name="home"),
+    url(r'^salary/$', Salary.as_view(), name="salary"),
     url(r'^admin/', include(admin.site.urls)),
 )
