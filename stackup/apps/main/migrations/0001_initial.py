be39bd95a698e3ea7ca10587a6b3103be4a106a7
James Pulec <jpulec@gmail.com>
2014-02-01 02:26:10 -0600

    CL scraping

--- stackup/apps/main/migrations/0001_initial.py
+++ stackup/apps/main/migrations/0001_initial.py
@@ -8,13 +8,50 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'Region'
+        db.create_table(u'main_region', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('cl_id', self.gf('django.db.models.fields.IntegerField')()),
+            ('cl_rent', self.gf('django.db.models.fields.IntegerField')(null=True)),
+            ('star_difference', self.gf('django.db.models.fields.IntegerField')(null=True)),
+        ))
+        db.send_create_signal(u'main', ['Region'])
+
+        # Adding model 'StandardOfLiving'
+        db.create_table(u'main_standardofliving', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('region', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.Region'])),
+            ('star_level', self.gf('django.db.models.fields.IntegerField')()),
+            ('threshold', self.gf('django.db.models.fields.FloatField')()),
+        ))
+        db.send_create_signal(u'main', ['StandardOfLiving'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'Region'
+        db.delete_table(u'main_region')
+
+        # Deleting model 'StandardOfLiving'
+        db.delete_table(u'main_standardofliving')
+
 
     models = {
-        
+        u'main.region': {
+            'Meta': {'object_name': 'Region'},
+            'cl_id': ('django.db.models.fields.IntegerField', [], {}),
+            'cl_rent': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'star_difference': ('django.db.models.fields.IntegerField', [], {'null': 'True'})
+        },
+        u'main.standardofliving': {
+            'Meta': {'object_name': 'StandardOfLiving'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['main.Region']"}),
+            'star_level': ('django.db.models.fields.IntegerField', [], {}),
+            'threshold': ('django.db.models.fields.FloatField', [], {})
+        }
     }
 
     complete_apps = ['main']
