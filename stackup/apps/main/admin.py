0b352cd67a413e38a88a36c4c6a39bb3b63fc0a6
James Pulec <jpulec@gmail.com>
2014-01-31 21:55:10 -0600

    Add models

--- stackup/apps/main/admin.py
+++ stackup/apps/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from stackup.apps.main.models import *
+
+admin.site.register(Region)
+admin.site.register(StandardOfLiving)
+
--- stackup/apps/main/models.py
+++ stackup/apps/main/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+class Region(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+class StandardOfLiving(models.Model):
+    region = models.ForeignKey('Region')
+    star_level = models.IntegerField()
+    threshold = models.FloatField()
+
+    def __unicode__(self):
+        return unicode(self.region) + ":" + unicode(self.star_level) + ":" + unicode(self.threshold)
